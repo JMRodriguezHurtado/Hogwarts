06960e49e354bb3f6ee6cac61ba4d9f5cbd57818
JMRodriguezHurtado <jmrodhurtado1989@hotmail.com>
2023-09-21 12:07:19 -0600

    updates on the DB

--- Howartsapp/hogwartshp/Server/src/index.js
+++ Howartsapp/hogwartshp/Server/src/index.js
@@ -1,31 +1,8 @@
-// EXPRESS
-
-const express = require("express");
-const mainRouter = require("./routes/mainRouter");
-const server = express();
-const morgan = require("morgan");
-
+const app = require("./app");
+const {conn} = require("./DB_connection");
 const PORT = 3001;
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
+  conn.sync({force: true});
   console.log(`Server raised on port: ${PORT}`);
 });
-
-server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
-
-server.use(express.json());
-server.use(morgan("dev"));
-
-//http://localhost:3001/rickandmorty
-server.use("/rickandmorty", mainRouter);
-
-module.exports = server;
